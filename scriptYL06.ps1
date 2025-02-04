25eb79b6cd7de4c7d6d88b49df404373d13c28ce
MarcusHKHK <marcus@hkhk.edu.ee>
2025-02-04 14:57:04 -0800

    h

---
+++ scriptYL06.ps1
@@ -0,0 +1,13 @@
+﻿#Marcus Krutto IT24
+[string]$essjapnimi = 'Marcus Krutto'
+[string]$minueemail = 'marcus@hkhk.edu.ee'
+[datetime]$kuupaev | get-date
+
+$essjapnimi
+$minueemail
+
+$skriptiAsukoht = 'C:\Users\Administrator\Documents\GitHub\Powershell'
+
+$skriptiAsukoht = $MyInvocation.MyCommand.Path
+$dir = Split-Path $skriptiAsukoht
+$emailid = Get-Content -path $dir\emailid.txt
