687cd180c9a8736e6b80776b4ad845b5d26cb90a
MarcusHKHK <marcus@hkhk.edu.ee>
2025-02-04 15:29:26 -0800

    Update scriptYL06.ps1

--- scriptYL06.ps1
+++ scriptYL06.ps1
@@ -1,6 +1,6 @@
 ﻿#Marcus Krutto IT24
-[string]$essjapnimi = 'Marcus Krutto'
-[string]$minueemail = 'marcus@hkhk.edu.ee'
+$essjapnimi = 'Marcus Krutto'
+$minueemail = 'marcus@hkhk.edu.ee'
 [datetime]$kuupaev | get-date
 
 $essjapnimi
@@ -11,3 +11,12 @@ $skriptiAsukoht = 'C:\Users\Administrator\Documents\GitHub\Powershell'
 $skriptiAsukoht = $MyInvocation.MyCommand.Path
 $dir = Split-Path $skriptiAsukoht
 $emailid = Get-Content -path $dir\emailid.txt
+
+$esimene =$emailid.Split(',')[0]
+$viimane =$emailid.Split(',')[-1]
+$test = $emailid.Split(',')
+$testimine =[array]$test
+$counter = $testimine.Length
+$esimene
+$viimane
+Write-Host "Emaile on siin $counter tükki"
