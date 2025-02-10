d62ec34181f99b914d0bc7e22e0ab74e6dba43f4
MarcusHKHK <marcus@hkhk.edu.ee>
2025-02-10 11:22:47 -0800

    Create yl08.ps1

---
+++ yl08.ps1
@@ -0,0 +1,15 @@
+﻿function ring-pind($radius=0){
+    [Math]::Pow($radius,2)*[Math]::PI
+    }
+
+ring-pind -radius 3
+
+function tyhikud($nimi="Vaikimisi nimi"){
+    $uusnimi = $nimi -replace "\s+" ," "
+    $uusnimi.Trim(" ");
+    $uusnimi.TrimEnd(" ");
+    $uusnimi.TrimStart(" ");
+    "Siin on parantatud nimi: $uusnimi"
+}
+
+tyhikud -nimi "    Marcus                    "
