08ab1c16901990d99b49511dc62dc5e91e37b70c
MarcusHKHK <marcus@hkhk.edu.ee>
2025-02-13 15:22:06 -0800

    no more powershel pleaseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ yl10.ps1
@@ -0,0 +1,7 @@
+﻿$xml = [xml](Get-Content C:\scripts\customers.xml)
+$rows = $xml.customers.customer
+foreach ($row in $rows){
+    if($row.country -contains 'Poland'){
+        $row.full_name
+    }
+}
